b7dfc43bb96c9aa36f33afe762766ba6b2d4d77b
lvzeyu <lvzeyu1995@gmail.com>
2023-05-24 12:19:34 +0900

    loop内容更新

--- css_tohoku/_build/jupyter_execute/draft/tips.py
+++ css_tohoku/_build/jupyter_execute/draft/tips.py
@@ -23,13 +23,6 @@
 # 
 # ```https://github.com/lvzeyu/chss_2023_assignment```から最新の状況を同期する操作を例として説明します。
 
-# - ```cd ~/chss_2023_assignment```
-# - ```git remote -v```でgitリポジトリに関連付けられたリモートリポジトリの一覧とそのURLを表示する
-# - ```origin/upstream  git@github.com:lvzeyu/chss_2023_assignment.git```というリモートリポジトリが表示されることを確認
-# - ```git branch <branch_name>```で自分のブランチを作成・移動
-# - ```git pull origin main```または ```git pull upstream main```でローカルリポジトリブランチをリポジトリの最新状態に更新します。
-# 
-
 # ````{tab-set}
 # 
 # ```{tab-item} ターミナル操作
